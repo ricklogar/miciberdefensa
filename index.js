0ce3af8df2c33d801fb714a6a2e308a2abb77323
JoseMa <esparev@hotmail.com>
2022-11-19 00:18:34 -0600

    refactor: ♻️ show results section in HTML after form click

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const startBtn = document.querySelector('#Home-div__Button');
+const endBtn = document.querySelector('#Questions-div__button');
+const resultsSection = document.querySelector('#Results');
+const arSection = document.querySelector('#Additional-Recommendations');
 
 startBtn.addEventListener('click', () => {
   document.querySelector('#Questions').scrollIntoView();
+});
+
+endBtn.addEventListener('click', () => {
+  resultsSection.classList.remove('hide-section');
+  arSection.classList.remove('hide-section');
+  resultsSection.scrollIntoView();
 });
