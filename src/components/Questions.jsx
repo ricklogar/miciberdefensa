4a44559488490638a48483cea96f146855f563f2
JoseMa <esparev@hotmail.com>
2022-11-24 00:23:51 -0600

    refactor: ♻️ made inputs required and semantic hmtl

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -4,75 +4,75 @@ import '@styles/Questions.css';
 const Questions = () => {
   return (
     <section id='Questions'>
-      <div className='Questions-div'>
+      <form className='Questions-div'>
         <div className='Questions-div__Q'>
           <p>¿Cuál es tu nombre?</p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q1' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q1' required />
         </div>
         <div className='Questions-div__Q'>
           <p>¿En qué día naciste?</p>
-          <input type='number' name='' placeholder='Ingrese su respuesta' id='Input-Q2' />
+          <input type='number' name='' placeholder='Ingrese su respuesta' id='Input-Q2' required />
         </div>
         <div className='Questions-div__Q'>
           <p>¿En qué año naciste?</p>
-          <input type='number' name='' placeholder='Ingrese su respuesta' id='Input-Q3' />
+          <input type='number' name='' placeholder='Ingrese su respuesta' id='Input-Q3' required />
         </div>
         <div className='Questions-div__Q'>
           <p>¿Cuál es tu apodo?</p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q4' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q4' required />
         </div>
         <div className='Questions-div__Q'>
           <p>
             ¿Como se llamó tu primera mascota? <span>(En caso de que aplique)</span>
           </p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q5' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q5' required />
         </div>
         <div className='Questions-div__Q'>
           <p>¿Cuál es el nombre de tu familiar más cercano?</p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q6' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q6' required />
         </div>
         <div className='Questions-div__Q'>
           <p>
             ¿Cuál es el nombre de tu pareja? <span>(En caso de que aplique)</span>
           </p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q7' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q7' required />
         </div>
         <div className='Questions-div__Q'>
           <p>¿Cuál es el nombre de tu lugar favorito?</p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q8' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q8' required />
         </div>
         <div className='Questions-div__Q'>
           <p>¿Cuál es tu animal favorito?</p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q9' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q9' required />
         </div>
         <div className='Questions-div__Q'>
           <p>¿Cuál es tu comida favorita?</p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q10' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q10' required />
         </div>
         <div className='Questions-div__Q'>
           <p>
             ¿Cuál es el nombre de alguno de tus hijos? <span>(En caso de que aplique)</span>
           </p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q11' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q11' required />
         </div>
         <div className='Questions-div__Q'>
           <p>¿Cuál es tu personaje de ficción favorito?</p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q12' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q12' required />
         </div>
         <div className='Questions-div__Q'>
           <p>¿Cuál es tu equipo de deportes favorito?</p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q13' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q13' required />
         </div>
         <div className='Questions-div__Q'>
           <p>¿Cuál es el nombre de la ciudad en la que naciste?</p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q14' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q14' required />
         </div>
         <div className='Questions-div__Q'>
           <p>¿Cuál es tu signo zodiacal?</p>
-          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q15' />
+          <input type='text' name='' placeholder='Ingrese su respuesta' id='Input-Q15' required />
         </div>
-        <button id='Questions-div__button'>Finalizar</button>
-      </div>
+        <button id='Questions-div__button' type='button'>Finalizar</button>
+      </form>
     </section>
   );
 };
