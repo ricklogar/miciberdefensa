c74ab26d55a3ea330e44de53ed63324052c89534
JoseMa <esparev@hotmail.com>
2022-12-01 14:44:00 -0600

    refactor: ♻️ add more devs to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,13 +12,30 @@ const Footer = () => {
           target='_blank'
           rel='noopener noreferrer'>
           Ricardo López
-        </a>{' '}y{' '}
+        </a>{' '}
+        ,{' '}
         <a
           className='footer__link'
           href='https://linkedin.com/in/esparev'
           target='_blank'
           rel='noopener noreferrer'>
           JoséMa Esparza
+        </a>{' '}
+        ,{' '}
+        <a
+          className='footer__link'
+          href='https://www.linkedin.com/in/eduardo-alejandro-rosales-acevedo-a7a763200/'
+          target='_blank'
+          rel='noopener noreferrer'>
+          Eduardo Rosales
+        </a>{' '}
+        y{' '}
+        <a
+          className='footer__link'
+          href='https://www.linkedin.com/in/david-alejandro-due%C3%B1as-arciga-750089200/'
+          target='_blank'
+          rel='noopener noreferrer'>
+          David Dueñas
         </a>
       </p>
     </footer>
